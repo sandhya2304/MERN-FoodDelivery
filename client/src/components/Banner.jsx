862509cc57807b6c843268edef7272497ce9612d
Sandhyua Sharma <sandhyasharma0423@gmail.com>
2024-09-01 00:10:57 +0530

    new file

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='max-w-screen-2xl container mx-auto xl:px-24 bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%'>
-        Banner
+    <div className='max-w-screen-2xl container mx-auto xl:px-24 bg-gradient-to-r 
+            from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%'>
+       <div className='py-24'>
+          banner
+       </div>
     </div>
   )
 }
